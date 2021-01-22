3adb56dcd816b141b0fbe9b01474a830dfba8ee5
Kshitij Patil <kshitijpatil98@gmail.com>
2021-01-22 17:01:04 +0530

    Minor UI changes

--- app/src/main/java/com/kshitijpatil/imeexp/MainFragment.kt
+++ app/src/main/java/com/kshitijpatil/imeexp/MainFragment.kt
@@ -5,17 +5,18 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.view.ViewGroup.LayoutParams.MATCH_PARENT
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.OutlinedTextField
 import androidx.compose.material.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.ComposeView
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.fragment.app.Fragment
+import com.kshitijpatil.imeexp.ui.theme.ImeGlitchTheme
 import dev.chrisbanes.accompanist.insets.AmbientWindowInsets
 import dev.chrisbanes.accompanist.insets.ProvideWindowInsets
 import dev.chrisbanes.accompanist.insets.ViewWindowInsetObserver
@@ -36,16 +37,26 @@ class MainFragment: Fragment() {
         // The WindowInsets instance is returned to us.
         val windowInsets = observer.start()
         setContent {
-            Providers(AmbientWindowInsets provides windowInsets) {
-                var text by remember { mutableStateOf(TextFieldValue()) }
-                Box(modifier = Modifier.fillMaxSize()) {
-                    OutlinedTextField(
-                        value = text,
-                        onValueChange = { text = it },
-                        modifier = Modifier
+            ImeGlitchTheme {
+                Providers(AmbientWindowInsets provides windowInsets) {
+                    var text by remember { mutableStateOf(TextFieldValue()) }
+                    Box(modifier = Modifier.fillMaxSize()) {
+                        Column(modifier = Modifier
                             .align(Alignment.BottomCenter)
                             .navigationBarsPadding()
-                    )
+                        ) {
+                            Text(
+                                "Click Here \u2193",
+                                style = MaterialTheme.typography.h5,
+                                color = Color.White,
+                                //modifier = Modifier.fillMaxWidth().wrapContentWidth()
+                            )
+                            OutlinedTextField(
+                                value = text,
+                                onValueChange = { text = it },
+                            )
+                        }
+                    }
                 }
             }
         }
